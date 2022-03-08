991dbc87d46c90e4d882f5c6221f69200ef16a8c
kiko-serra <franciscopimentelserra@gmail.com>
2022-03-08 12:32:32 +0000

    descricao da solucao do 4

--- 01/P01/ex4.cpp
+++ 01/P01/ex4.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 
 int main() {
+    //usar operador de bites
+    //fazer operacao de and para ver qual o valor do bit 
+    //mais a direita e depois sucessivos shfts 
+    // a soma dos bites que si é o numero de 1
+    // ones += s & f -> and 
   return 0;
 }
