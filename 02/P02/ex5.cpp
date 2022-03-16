08dc6cf6743b88f19bc14121b0e64d428024ba50
kiko-serra <franciscopimentelserra@gmail.com>
2022-03-16 22:18:42 +0000

    done some improving to finish ex5

--- 02/P02/ex5.cpp
+++ 02/P02/ex5.cpp
@@ -1,55 +1,48 @@
 #include<iostream>
 using namespace std;
 
-/*unsigned long bell(unsigned long n);
-int main(){
-       
+int bellNumber(int n);
+
+int main()
+{
+   for (int n=0; n<=5; n++){
+       int res = bellNumber(n);
+      cout << "Bell Number " << n << " is "
+           << res << endl;
+           }
+   return 0;
 }
 
-unsigned long bell(unsigned long n){
-    if (n == 0 || n == 1)
-    {
-        return n;
-    }
-    else{
-        int up = 1;
-        int down = 1;
-        for (int i = 0; i < n; i++)
-        {
-            up *= (n-i+1);
-            down *= ()
-        }
-        
-    }
-}
-*/
- 
 int bellNumber(int n)
 {
    int particoes[n][n];
    particoes[0][0] = 1;
-   for (int i=1; i<=n; ++i)
+   for (int a = 0; a < n; a++)
    {
-       particoes[i][i+1]=0;
+       for (int b = 0; b < n; b++)
+       {
+           particoes[a][b]=0; // every number in the array is 0
+       }
    }
-   for (int i = 1; i < n; ++i)
-   {
-       int linha_ant[i-1];
-       linha_ant[0]=1;
-      for (int j = 1; j < i; ++j)
-      {
-        linha_ant[j]=particoes[i-1][j];
+   for (int i = 1; i < n; ++i){
+        int linha_ant[i-1];
+       if (i==2){
+           linha_ant[0]=1; //first line inn triangle =1
+       }
+      for (int j = 1; j < i; j++){
+        linha_ant[j-1]=particoes[i-2][j-1]; // line before equals to particoes[i-1]
+        cout << i << "-i " << j << "-j "<< endl;
       }
-   }
-   
-   return particoes[n][0];
-}
- 
-// Driver program
-int main()
-{
-   for (int n=0; n<=5; n++)
-      cout << "Bell Number " << n << " is "
-           << bellNumber(n) << endl;
-   return 0;
+      for (int k = 0; k < i; k++){
+          if (!k)
+          {
+            particoes[i][k]=linha_ant[i-1]; //first column equals to last column from upper row
+          }
+          else{
+            particoes[i][k]=particoes[i][k-1]+linha_ant[k-1]; // actual number equals number before plus number befor in upper row
+          }
+           cout << i << "-i " << k << "-k "<< endl;
+      }
+    }
+   return particoes[n-1][0];
 }
