0b4370debd3fdbe08f28c927466f8a8278163fda
kiko-serra <franciscopimentelserra@gmail.com>
2022-03-27 22:50:35 +0100

    doing ex4 and good idea on how to do ex5

--- 03/P03/ex4.cpp
+++ 03/P03/ex4.cpp
@@ -2,52 +2,57 @@
 using namespace std;
 
 void rleEncode(const char str[], char rle[]);
-int length(const char str[]) ;
+int length(const char str[]);
 
 int main()
 {
-    /*
     char rle[1] = {-1};
     rleEncode("", rle);
-    cout << rle << endl;
-*/
-    char rle[2 + 1] = {-1, -1, -1};
-    rleEncode("a", rle);
-    cout << rle << endl;
-/*
-    char rle[10 + 1] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-    rleEncode("abcde", rle);
-    cout << rle << endl;
+    for (int i = 0; i < 1; i++)
+    { 
+        cout << rle[i];
+    }
+    cout << endl;
+    /*
+        char rle[2 + 1] = {-1, -1, -1};
+        rleEncode("a", rle);
 
-    char rle[9 + 1] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-    rleEncode("aaaaabbbbbbbbbbbcccd", rle);
-    cout << rle << endl;
+        char rle[10 + 1] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+        rleEncode("abcde", rle);
 
-    char rle[3 + 1] = {-1, -1, -1, -1};
-    rleEncode("xxxxxxxxxxxxxxxxxxxx", rle);
-    cout << rle << endl;
-    */
+
+        char rle[9 + 1] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+        rleEncode("aaaaabbbbbbbbbbbcccd", rle);
+                    char rle[3 + 1] = {-1, -1, -1, -1};
+                    rleEncode("xxxxxxxxxxxxxxxxxxxx", rle);
+                    cout << rle << endl;
+                    */
+    return 0;
 }
 
-void rleEncode(const char str[], char rle[]){
+void rleEncode(const char str[], char rle[])
+{
     int tamanho = length(str);
-    for (int i = 0; i < tamanho; i++) {
+    int aux = 0;
+    for (int i = 0; i < tamanho; i++)
+    {
 
         int count = 1;
-        while (i < tamanho - 1 && str[i] == str[i + 1]) {
+        while (i < tamanho - 1 && str[i] == str[i + 1])
+        {
             count++;
             i++;
         }
- 
-        // Print character and its count
-        cout << str[i] << count;
+        rle[aux++] = '0' + count;
+        rle[aux++] = str[i];
+        cout << str[i] << count << " " << aux << endl;
     }
-    
 }
 
-int length(const char str[]) {
-  int l = 0;
-  while (str[l] != '\0') 
-    l++;
-  return l;
+int length(const char str[])
+{
+    int l = 0;
+    while (str[l] != '\0')
+        l++;
+    return l;
 }
