8edadd4f28446e47fadf3adb82507d3f78caf535
kiko-serra <franciscopimentelserra@gmail.com>
2022-03-30 23:35:39 +0100

    Continued ex3

--- 04/P04/ex3.cpp
+++ 04/P04/ex3.cpp
@@ -6,27 +6,29 @@ dlnode *insert(dlnode *dln, int k, int v);
 
 int main()
 {
-    dlnode *dln = build(1, build(2, build(3, build(4))));
-    dln = insert(dln, 3, 0);
+    dlnode *dln = nullptr;
+    dln = insert(dln, 0, 0);
     print(dln);
     destroy(dln);
+    /*
+        dlnode *dln = build(1, build(2, build(3, build(4))));
+        dln = insert(dln, 3, 0);
+        print(dln);
+        destroy(dln);
+        */
 }
 
 dlnode *insert(dlnode *dln, int k, int v)
 {
     dlnode *curr = dln;
     dlnode *prev = nullptr;
-    dlnode *aux = new dlnode;
-    aux->value = v;
-    aux->prev = nullptr;
-    aux->next = nullptr;
     int i = 0;
     if (!k)
     {
-        dlnode* new_node={v, nullptr, dln};
-        dln->prev=new_node;
+        dlnode *new_node = new dlnode {v, nullptr, dln};
+        dln->prev = new_node;
     }
-    
+
     while (curr != nullptr && i < k)
     {
         prev = curr;
@@ -35,23 +37,14 @@ dlnode *insert(dlnode *dln, int k, int v)
     }
     if (curr != nullptr)
     {
-        if (i == k)
-        {
-            /*
-            if (curr->next == nullptr)
-            {
-                aux->next = curr->next;
-                aux->prev = curr;
-                curr->next = aux;
-            }*/
-            prev->next =  new_node{v, prev, curr};
-            curr->prev = prev->next;
-        }
+        dlnode *new_node2 = new dlnode{v, prev, curr};
+        prev->next = new_node2;
+        curr->prev = prev->next;
     }
-    else if(i==k){
-        aux->next = curr->next;
-        aux->prev = curr;
-        curr->next = aux;
+    if (i == k)
+    {
+        dlnode *new_node3 = new dlnode{v, curr, nullptr};
+        curr->next = new_node3;
     }
     return dln;
 }
