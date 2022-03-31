dd1f227b0c618ca2c69c9fc674369af245796aae
kiko-serra <franciscopimentelserra@gmail.com>
2022-03-31 12:16:07 +0100

    To-do ex4 & ex5

---
+++ 04/P04/ex4.cpp
@@ -0,0 +1,16 @@
+#include "etree.cpp"
+#include<iostream>
+
+using namespace std;
+
+int eval(const etree* t);
+
+int main(){
+
+
+    return 0;
+}
+
+int eval(const etree* t){
+
+}
---
+++ 04/P04/ex5.cpp
@@ -0,0 +1,16 @@
+#include "svnode.cpp"
+#include<iostream>
+
+using namespace std;
+
+svnode* sum(const svnode* a, const svnode* b);
+
+int main (){
+
+    return 0;
+}
+
+svnode* sum(const svnode* a, const svnode* b){
+
+
+}
