208cf6b39afc6b3f71a338519f7ef1361fdc8f59
kiko-serra <franciscopimentelserra@gmail.com>
2022-05-23 19:03:17 +0100

    Done ex4 from 09

--- 09/P09/ex4/Pair.h
+++ 09/P09/ex4/Pair.h
@@ -2,53 +2,66 @@
 #define PAIR_H
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
-template<typename T1, typename T2>
-class Pair{
-    public:
-        Pair(const T1& first, const T2& second);
-        T1 get_first() const;
-        T2 get_second() const;
-        void show() const;
-
-    private:
-        T1 first_;
-        T2 second_;
+template <typename T1, typename T2>
+class Pair {
+public:
+    Pair(const T1& first, const T2& second);
+    T1 get_first() const;
+    T2 get_second() const;
+    void show() const;
+    static bool is_less(const Pair<T1,T2>& a, const Pair<T1,T2>& b, const int& key);
+private:
+    T1 first_;
+    T2 second_;
 };
 
-template<typename T1, typename T2>
-Pair<T1, T2>::Pair(const T1& first, const T2& second): first_(first), second_(second){}
+template <typename T1, typename T2>
+Pair<T1,T2>::Pair(const T1& first, const T2& second):first_(first), second_(second){}
 
-template<typename T1, typename T2>
-T1 Pair<T1, T2>::get_first() const{
+template <typename T1, typename T2>
+T1 Pair<T1,T2>::get_first() const{
     return first_;
 }
 
-template<typename T1, typename T2>
-T2 Pair<T1, T2>::get_second() const{
+template <typename T1, typename T2>
+T2 Pair<T1,T2>::get_second() const{
     return second_;
 }
 
-template<typename T1, typename T2>
-void Pair<T1, T2>::show() const{
-    cout << "(" << get_first() << "," << get_second() << ")";
+template <typename T1, typename T2>
+void Pair<T1,T2>::show() const{
+    cout << "{" << first_ << "," << second_ << "}";
 }
 
-void sort_by_first(vector<Pair<typename T1, typename T2>>& v){
-    sort(v.begin(), v.end(), [](const Pair<T1, T2>& p1, const Pair<T1, T2>& p2));
+bool is_less_first(const Pair<string,int>& a, const Pair<string,int>& b){
+    return a.get_first()<b.get_first();
 }
 
-void sort_by_second(vector<Pair<typename T1, typename T2>>& v){
-    sort(v.begin(), v.end(), [](const Pair<T1, T2>& p1, const Pair<T1, T2>& p2));
+bool is_less_second(const Pair<string,int>& a, const Pair<string,int>& b){
+    return a.get_second()<b.get_second();
 }
 
-void show(const vector<Pair<typename T1, typename T2>>& v){
+void sort_by_first(vector<Pair<string,int>>& persons){
+    sort(persons.begin(),persons.end(),is_less_first);
+}
+
+void sort_by_second(vector<Pair<string,int>>& persons){
+    sort(persons.begin(),persons.end(),is_less_second);
+}
+
+void show(vector<Pair<string,int>> persons){
     cout << "{";
-    for(const Pair<T1, T2>& p: v){
+    for(Pair<string,int> p : persons)
+    {
         p.show();
     }
-    cout << "}"<< endl;
+    cout << "}";
 }
+
 #endif
