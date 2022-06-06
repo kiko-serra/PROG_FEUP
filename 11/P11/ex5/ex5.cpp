992e468cf9b0162fccf1916ca3b9b3b4d1014836
kiko-serra <franciscopimentelserra@gmail.com>
2022-06-07 00:04:15 +0100

    Done all from P11

--- 11/P11/ex5/ex5.cpp
+++ 11/P11/ex5/ex5.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 #include "Employee.h"
 #include "HourlyEmployee.h"
 #include "SalariedEmployee.h"
 
 using namespace std;
 
-void read_hours_worked(vector<Employee *> &employees){
-    for (size_t i = 0; i < employees.size(); i++){
-        if (employees[i]->get_rate()){
-            double hours;
-            cin >> hours;
-            ((HourlyEmployee *)employees[i])->set_hours(hours);
+void HourlyEmployee::calculate_net_pay(){
+     set_net_pay (wage_rate_ * hours_);
+}
+void SalariedEmployee::calculate_net_pay(){
+    set_net_pay (salary_);
+}
+void read_hours_worked(vector<Employee*> &employees){
+    for ( Employee* temp : employees){
+        HourlyEmployee* worker = dynamic_cast<HourlyEmployee*>(temp); 
+        if ( worker != nullptr){
+            double temp2;
+            cin >> temp2;
+            worker->set_hours(temp2);
         }
     }
 }
-
 void calculate_pay(vector<Employee*>& employees){
-    for (size_t i = 0; i < employees.size(); i++){
-        employees[i]->calculate_net_pay();
+    for ( Employee* temp : employees){
+        temp->calculate_net_pay();
     }
 }
-
 void print_checks(const vector<Employee*>& employees){
-    cout<<"|";
-    for (size_t i = 0; i < employees.size(); i++){
+    cout << "|";
+    for ( Employee* temp : employees){
         cout << " ";
-        employees[i]->print_check();
-        cout << " ";
+        HourlyEmployee* worker = dynamic_cast<HourlyEmployee*>(temp);
+        if (worker != nullptr){
+            cout <<fixed << setprecision(2)<< worker->get_name()<<":HE("
+                << worker->get_hours()<<','
+                <<worker->get_rate()<<")=" << worker->get_net_pay() << " |";
+        }
+        else{
+            SalariedEmployee* worker = dynamic_cast<SalariedEmployee*>(temp);
+            cout <<fixed << setprecision(2)<< worker->get_name()<< ":SE("<<
+                    worker->get_salary()<< ")=" << 
+                    worker->get_net_pay()<< " |";
+        }
     }
-    cout << "|" << endl;
 }
