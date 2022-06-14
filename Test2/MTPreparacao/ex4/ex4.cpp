691f8f20f778c1056e3db380f2d98901e252820e
kiko-serra <franciscopimentelserra@gmail.com>
2022-06-14 16:32:38 +0100

    Done ex4 from Preparation test

--- Test2/MTPreparacao/ex4/ex4.cpp
+++ Test2/MTPreparacao/ex4/ex4.cpp
@@ -4,3 +4,21 @@
 #include <iostream>
 #include <climits>
 using namespace std;
+
+string cat_keys(list<map<string, unsigned>> lst){
+    unsigned min = UINT_MAX;
+    map<string, unsigned> aux;
+    string result;
+    for(auto &m : lst){
+        for(auto &entry : m){
+            if(entry.second<min){
+                min = entry.second;
+                aux = m;
+            }
+        }
+    }
+    for(auto &entry : aux){
+        result += entry.first;
+    }
+    return result;
+}
